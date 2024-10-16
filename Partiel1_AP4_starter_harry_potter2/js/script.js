8afbbefc4a113e960ff7ca34974ca760d7253692
Yvi <yvi.destombes@icloud.com>
2024-10-16 11:19:11 +0200

    Partie 2 Quesion 2 : Tri par maison des personnages

--- Partiel1_AP4_starter_harry_potter2/js/script.js
+++ Partiel1_AP4_starter_harry_potter2/js/script.js
@@ -1,14 +1,7 @@
-async function fetchCharacters() {
-    const response = await fetch("https://hp-api.onrender.com/api/characters");
-    const data = await response.json();
-    const limitedCharacters = data.slice(0, 12);
-    const charactersHtml = limitedCharacters.map(character => createCharacterCard(character)).join('');
-    charactersContainer.innerHTML = charactersHtml;
-}
-
-
+const apiUrl = 'https://hp-api.onrender.com/api/characters';
 const charactersContainer = document.querySelector('.characters');
-
+const houseButtons = document.querySelectorAll('.houses > div');
+let allCharacters = [];
 
 function createCharacterCard(character) {
     let houseClass = '';
@@ -38,5 +31,23 @@ function createCharacterCard(character) {
   `;
 }
 
+function displayCharacters(characters) {
+    const charactersHtml = characters.map(character => createCharacterCard(character)).join('');
+    charactersContainer.innerHTML = charactersHtml;
+}
+
+function filterByHouse(house) {
+    const filteredCharacters = allCharacters.filter(character => character.house === house);
+    displayCharacters(filteredCharacters);
+}
+
+async function fetchCharacters() {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        allCharacters = data.slice(0, 12);
+        displayCharacters(allCharacters);
+}
+
+houseButtons.forEach(button => {button.addEventListener('click', (event) => {const houseName = event.currentTarget.getAttribute('data-house');filterByHouse(houseName);});});
 
 fetchCharacters();
